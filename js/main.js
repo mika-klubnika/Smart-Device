e0748fb093eb31058aa9a817f01a5ae79681fe26
Марина <mika-klubnika@yandex.ru>
2021-09-19 20:24:09 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,26 +1,94 @@
-const accordion = document.querySelectorAll('.accordion');
-// const accordionBlock = document.querySelectorAll('.accordion__block');
+'use strict';
 
+const accordion = document.querySelector('.accordion');
+const accordionButton = accordion.querySelectorAll('.accordion__button');
+const accordionItems = accordion.querySelectorAll('.accordion__item');
 
-accordion.forEach((elem) => {
-  elem.addEventListener('click', function () {
+if(accordion && accordionButton && accordionItems) {
+  function toggleAccordion () {
+    const thisItem = this.parentNode;
 
-    const accordionBlockActive = document.querySelectorAll('.accordion__block--active');
+    accordionItems.forEach(item => {
+      if (thisItem == item) {
+        thisItem.classList.toggle('accordion__item--active');
+        return;
+      }
+      item.classList.remove('accordion__item--active');
+    });
+  }
 
-    accordionBlockActive.forEach((elem) => {
-      elem.classList.remove('accordion__block--active');
-    })
+  accordionButton.forEach(block => block.addEventListener('click', toggleAccordion));
+}
 
-    const parentElem = this.parentNode;
+'use strict';
 
-    const accordionBlock = parentElem.querySelector('.accordion__block');
+const inputPhones = document.querySelectorAll('[type=tel]');
 
-    if (accordionBlock.classList.contains('accordion__block--active')) {
-      accordionBlock.classList.toggle('accordion__block--active');
-      accordionToggle.classList.toggle('accordion__toggle--close');
+if(inputPhones) {
+  for (let i = 0; i < inputPhones.length; i++) {
+    new IMask(inputPhones[i], {
+      mask: '+7(000)000-00-00',
+    });
+  }
+}
+
+'use strict';
+
+(() => {
+  const modal = document.querySelector('.modal');
+  const modalButton = document.querySelector('.modal-button');
+  const modalBase = modal.querySelector('.modal__base');
+  const modalClose = modal.querySelector('.modal__close');
+  const form = modal.querySelector('form');
+  const userName = modal.querySelector('#user-name');
+  const phone = modal.querySelector('[type=tel]');
+  const message = modal.querySelector('#message');
+  const isStorageSupport = true;
+  const storage = {};
+
+  try {
+    storage.name = localStorage.getItem('name');
+    storage.phone = localStorage.getItem('phone');
+    storage.message = localStorage.getItem('message');
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  modalButton.addEventListener('click', () =>{
+    modal.classList.add('modal--open');
+
+    if (storage.name) {
+      userName.value = storage.name;
+      phone.value = storage.phone;
+      message.value = storage.message;
+      message.focus();
+    } else {
+      userName.focus();
     }
-    else {
-      accordionBlock.classList.add('accordion__block--active');
+  });
+
+  modalBase.addEventListener('click', evt => {
+    evt.target === modalBase && modal.classList.remove('modal--open');
+  });
+
+  modalClose.addEventListener('click', () => {
+    modal.classList.remove('modal--open');
+  });
+
+  form.addEventListener('submit', () => {
+    if (isStorageSupport) {
+      localStorage.setItem('name', userName.value);
+      localStorage.setItem('phone', phone.value);
+      localStorage.setItem('message', message.value);
     }
-  })
-})
+  });
+
+  window.addEventListener('keydown', evt => {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (modal.classList.contains('modal--open')) {
+        modal.classList.toggle('modal--open');
+      }
+    }
+  });
+})();
