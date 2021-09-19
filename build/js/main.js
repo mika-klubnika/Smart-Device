2b150b5e79a9ce1174da73029427a26970d0977f
Марина <mika-klubnika@yandex.ru>
2021-09-19 03:30:30 +0300

    modal

--- build/js/main.js
+++ build/js/main.js
@@ -24,3 +24,30 @@ accordion.forEach((elem) => {
     }
   })
 })
+
+const modal = document.querySelector('.modal');
+const modalButton = document.querySelector('.modal-button');
+const modalClose = modal.querySelector('.modal__close');
+const modalBase = document.querySelector('.modal__base');
+
+
+modalButton.addEventListener('click', function () {
+  modal.classList.add('modal--open');
+});
+
+modalBase.addEventListener('click', evt => {
+  evt.target === modalBase && modal.classList.remove('modal--open');
+});
+
+modalClose.addEventListener('click', function () {
+  modal.classList.remove('modal--open');
+});
+
+window.addEventListener('keydown', evt => {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modal.classList.contains('modal--open')) {
+      modal.classList.toggle('modal--open');
+    }
+  }
+});
